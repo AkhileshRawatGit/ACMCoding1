0bb473c4faf7d69637d3e1eb5f54bfd41992b58f
Akhilesh Rawat <your-email@example.com>
2025-10-04 22:25:48 +0530

    project4

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = "http://localhost:8080/api"
+const API_BASE_URL =
+  window.location.hostname === "localhost" ? "http://localhost:8080/api" : `${window.location.origin}/api`
 let currentUser = null
 let currentQuestion = null
 let codeEditor = null
